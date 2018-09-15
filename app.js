56f4feb2d93086a11b305b19bff0197df0d23b2c
Sheng Jiang <jiang.she@husky.neu.edu>
2018-09-14 23:48:18 -0400

    basic function added
    roll, hold init end added

--- app.js
+++ app.js
@@ -8,3 +8,57 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+window.onload = init;
+document.getElementsByClassName("btn-new")[0].onclick = init;
+
+var rollBtn = document.getElementsByClassName("btn-roll")[0];
+rollBtn.onclick = rollDice;
+
+var holdBtn = document.getElementsByClassName("btn-hold")[0];
+holdBtn.onclick = hold;
+
+
+function rollDice() {
+  var num = Math.ceil(6 * Math.random());
+  var dice = document.getElementsByClassName("dice")[0];
+  dice.src = "dice-" + num + ".png";
+  if (num === 1) {
+    roundScore = 0;
+  } else {
+    roundScore += num;
+  }
+  var activePlayerCurrent = document.getElementById("current-" + activePlayer);
+  activePlayerCurrent.innerHTML = roundScore;
+}
+
+function hold() {
+  scores[activePlayer] += roundScore;
+  var activePlayerScore = document.getElementById("score-" + activePlayer);
+  activePlayerScore.innerHTML = parseInt(activePlayerScore.innerHTML) + scores[activePlayer];
+  if (activePlayerScore.innerHTML > 20) {
+    end();
+  }
+  activePlayer = 1 - activePlayer;
+}
+
+function init() {
+  activePlayer = 0;
+  scores = [0, 0];
+  roundScore = 0;
+  document.getElementById("current-0").innerHTML = 0;
+  document.getElementById("current-1").innerHTML = 0;
+  document.getElementById("score-0").innerHTML = 0;
+  document.getElementById("score-1").innerHTML = 0;
+  document.getElementsByClassName("dice")[0].src = "dice-1.png";
+}
+
+function end() {
+  window.alert("player " + activePlayer + " wins! New game will start!");
+  init();
+}
